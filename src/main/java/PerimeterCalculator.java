6b630a1927a0bdb3efcfeeb696c2ee8756237832
Luisa Quiceno <luisaquiceno1503@gmail.com>
2024-09-11 20:17:38 -0500

    prueba

--- src/main/java/PerimeterCalculator.java
+++ src/main/java/PerimeterCalculator.java
@@ -15,7 +15,7 @@ public class PerimeterCalculator {
 
     public double calculatePerimeter(Shape s) {
         // Definir la variable que acumule el perimetro
-
+        double perimeter=0;
         List<Point> points = s.getPoints();
         Point prevPoint = points.getLast();
 
@@ -23,27 +23,36 @@ public class PerimeterCalculator {
             // Obtener la distancia de los lados
             // Ayuda: prevPoint.distancia(point)
 
+            double distance=prevPoint.distancia(point);
+            perimeter+=distance;
             prevPoint = point;
         }
-
         // Retornar la variable del perimetro
+
+        return perimeter;
     }
 
     public int getTotalPoints(Shape s) {
-        // Definir la variable para el total de puntos
+        // Definir la variable para el total de puntos}
+        int totalPoints=0;
         for(Point point: s.getPoints()) {
+
             // Acumular el total de puntos
+            totalPoints++;
         }
 
         // Retornas el valor de puntos
+        return totalPoints;
     }
 
     public static void main (String[] args) {
         PerimeterCalculator perimeterCalculator = new PerimeterCalculator();
-        // Definit el nombre del archivo
+        // Definit el nombre del archivo}
+
+        String fileName= "square.txt";
         Shape shape = new Shape();
 
-        shape.readPoints("nombre_archivo");
+        shape.readPoints(fileName);
         // Mostrar el valor de calculatePerimeter y getTotalPoints
     }
 }
